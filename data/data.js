9372a7a6901e2c04f0a2cc81ad6ca04b8a8a1b5e
zanemit <zane.mitrevica@gmail.com>
2026-04-09 15:00:14 +0300

    first commit

---
+++ data/data.js
@@ -0,0 +1,14 @@
+const PALETTE = ["#ff4b50", "#4b0023", "#c8c8dc", "#cd4b82", "#ffa08c"];
+const LINESTYLES = ["solid", "dash", "dot", "dashdot"];
+
+const  mealPrices = {
+    '"Lidojošā varde"': [14.9, 29.9],
+    '"Lokāls Veldze"': [9.9, 14.0],
+    '"Kebab House"': [7.0, 8.9],
+    '"Stockpot"': [5.1, 9.5],
+    '"Borščs"': [5, 7],
+    '"Ezītis"': [4.9, 9.5],
+    '"Daily" @RSU': [3.7, 5.8],
+    '"Lido"': [3.6, 4.7],
+    '"Karote un Dakša"': [3.5, 5.0],
+};
